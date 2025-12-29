9448a30aa380e18c5a08d236b188ce3deff7ab99
MayMessaoudLamekh <may.messaoudlamekh@esprit.tn>
2025-12-29 12:46:47 +0100

    aa

--- src/utilisateurs/dto/create-utilisateur.dto.ts
+++ src/utilisateurs/dto/create-utilisateur.dto.ts
@@ -75,11 +75,5 @@ export class CreateUtilisateurDto {
   @IsOptional()
   @IsString()
   avatar?: string;
-  @ApiProperty({
-  example: '123456',
-  description: 'Mot de passe du compte utilisateur',
-})
-@IsString()
-password: string;
-
 }
+
